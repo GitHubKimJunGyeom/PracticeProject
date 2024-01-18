b4908d84f858537858b3dfe4097f635943175104
cento <>
2024-01-18 09:32:40 +0900

    테스트

---
+++ Assets/Script/Manager/UIManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class UIManager : MonoSingleton<UIManager>
+{
+    // DonDestroy Root -> Loading Bar(신 이동할 때마다 계속 나타 나야하므로 파괴시키지 않는다 )
+    //GameObject SubRoot = null;
+
+    // UI prefab 저장하는 dictionary
+    Dictionary<UI_TYPE, GameObject> uiDictionary = new Dictionary<UI_TYPE, GameObject>();
+
+    GameObject GetUI(UI_TYPE _uiType, bool isDontDestroy = false)
+    {
+        if (isDontDestroy == false)
+        {
+            if (uiDictionary.ContainsKey(_uiType) == true)
+            {
+                return uiDictionary[_uiType];
+            }
+        }
+
+
+        GameObject makeUI = null;
+        GameObject prefabUI = Resources.Load("Prefabs/UI/" + _uiType.ToString()) as GameObject;
+
+
+        if (prefabUI != null)
+        {
+            if (isDontDestroy == false)
+            {
+                makeUI = Instantiate(prefabUI);
+                makeUI.transform.SetParent(this.gameObject.transform);
+                uiDictionary.Add(_uiType, makeUI);
+            }
+
+            makeUI.SetActive(false);
+        }
+        return makeUI;
+    }
+
+    public GameObject ShowUI(UI_TYPE _uiType, bool isSub = false)
+    {
+        GameObject showObject = GetUI(_uiType, isSub);
+        if (showObject != null && showObject.activeSelf == false)
+        {
+            showObject.SetActive(true);
+        }
+        return showObject;
+    }
+
+
+    public void HideUI(UI_TYPE _uiType, bool isSub = false)
+    {
+        GameObject showObject = GetUI(_uiType, isSub);
+        if (showObject != null && showObject.activeSelf == true)
+        {
+            showObject.SetActive(false);
+        }
+    }
+
+    public void Clear()
+    {
+        foreach (KeyValuePair<UI_TYPE, GameObject> pair in uiDictionary)
+        {
+            Destroy(pair.Value);
+        }
+        uiDictionary.Clear();
+    }
+}
+
---
+++ Assets/UI_MainStage.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UI_MainStage : BaseObject
+{
+
+    private Transform mainPanel;
+    private Transform listPanel;
+    private Transform spawnPanel;
+    private Transform combinePanel;
+
+    private Button listButton;
+    private Button spawnButton;
+    private Button combineButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainPanel = FindInChild("panel");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void ListButtonClick()
+    {
+    }
+
+    private void SpawnButton()
+    {
+
+    }
+
+    private void CombineByttonClick()
+    {
+
+    }
+}
